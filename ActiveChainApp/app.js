6a1fd48cafb150c4436bf46b875f60804d7c6093
duxingzhou <duxingzhou@hotmail.com>
2018-08-15 21:16:39 +0800

    first ver

--- ActiveChainApp/app.js
+++ ActiveChainApp/app.js
@@ -1,23 +1,29 @@
 //app.js
 App({
+  globalData: {
+    isUserAuth: false,
+    userInfo: null,
+    appid: "wx5b455f3a9165c202",
+    appsecret: "cba16baa627a79ae55c2ca1323237e27"
+  },
 
   onLaunch: function() {
     var self = this
     // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        let authUrl = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + self.globalData.appid + '&secret=' + self.globalData.appsecret + '&js_code=' + res.code + '&grant_type=authorization_code';
-        wx.request({
-          url: authUrl,
-          data: {},
-          method: 'GET',
-          success: res => {
-            self.globalData.user.openid = res.data.openid;
-          }
-        })
-      }
-    })
+    // wx.login({
+    //   success: res => {
+    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    //     let authUrl = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + self.globalData.appid + '&secret=' + self.globalData.appsecret + '&js_code=' + res.code + '&grant_type=authorization_code';
+    //     wx.request({
+    //       url: authUrl,
+    //       data: {},
+    //       method: 'GET',
+    //       success: res => {
+    //         self.globalData.user.openid = res.data.openid;
+    //       }
+    //     })
+    //   }
+    // })
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -28,7 +34,6 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
@@ -39,12 +44,5 @@ App({
         }
       }
     })
-  },
-  globalData: {
-    user: null,
-    userInfo: null,
-    isUserAuth: false,
-    appid: "wx5b455f3a9165c202",
-    appsecret: "cba16baa627a79ae55c2ca1323237e27"
   }
 })
--- ActiveChainApp/pages/main/mainui.js
+++ ActiveChainApp/pages/main/mainui.js
@@ -1,6 +1,7 @@
 // pages/main/mainui.js
-Page({
+const app = getApp()
 
+Page({
   /**
    * 页面的初始数据
    */
@@ -18,77 +19,106 @@ Page({
       "swimming": "游泳",
       "walking": "步行"
     },
+    userInfo: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
-    this.setData({
-      isUserAuth: getApp().globalData.isUserAuth
-    })
+  onLoad: function (options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        isUserAuth: app.globalData.isUserAuth
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          isUserAuth: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          app.globalData.isUserAuth = true
+          this.setData({
+            userInfo: app.globalData.userInfo,
+            isUserAuth: app.globalData.isUserAuth
+          })
+        },
+        fail: res => {
+        }
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
-
+  onReady: function () {
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
-
+  onShow: function () {
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   },
 
-  goAelfIo: function() {
+  goAelfIo: function () {
     wx.navigateTo({
       url: '../web/webview?url=https://aelf.io',
-      fail: function() {},
-      complete: function() {}
+      fail: function () { },
+      complete: function () { }
     });
   },
 
-  bindGetUserInfo: function(e){
-    console.log(e.detail.userInfo)
-    console.log(e.detail.encryptedData)
-    console.log(e.detail.iv)
+  bindGetUserInfo: function (e) {
+    app.globalData.userInfo = e.detail.userInfo
+    app.globalData.isUserAuth = true
+    this.setData({
+      userInfo: app.globalData.userInfo,
+      isUserAuth: app.globalData.isUserAuth
+    })
   }
 })
